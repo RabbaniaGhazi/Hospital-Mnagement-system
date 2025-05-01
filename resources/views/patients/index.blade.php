5491ce47bd88d6b14d7417783d2cdc19a00e5b0d
RabbaniaGhazi <rabbaniazakir17@gmail.com>
2025-05-01 03:49:26 -0700

    vite reference removed

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patients List</title>
-    @vite('resources/css/app.css') <!-- Using Vite for asset management -->
-    <script src="https://cdn.jsdelivr.net/npm/lucide@latest/dist/umd/lucide.min.js"></script> <!-- Lucide for icons -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
         body {
             background-color: #f8f9fa;
